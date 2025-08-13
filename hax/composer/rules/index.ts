7e1d74113e020e291bae0b81455de78eec58988d
Chizuzu <izuzuchidumga@gmail.com>
2025-08-13 11:40:16 -0700

    delete and rename rules context folder

--- hax/composer/rules/index.ts
+++
@@ -1,5 +0,0 @@
-export { RulesModal } from "./rules";
-
-export { RulesProvider, useRules } from "./rules-context";
-export type { Rule, RulesState } from "./types";
-export { createStorage, storage } from "./localStorage";
