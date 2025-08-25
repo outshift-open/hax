4aed00c7f079462bd7ba77bda84e793cb1fe9df9
Chizuzu <izuzuchidumga@gmail.com>
2025-08-25 07:38:30 -0700

    extract adapter into sdk

---
+++ hax/adapter/index.ts
@@ -0,0 +1,11 @@
+export { HAXAdapter } from "./adapter";
+export type { HAXAdapterOptions, Protocol } from "./adapter";
+export type { CopilotEventStream, ProcessMessageActions } from "./agent.type";
+export {
+  logger,
+  useLogger,
+  createLogger,
+  ConsoleLogger,
+  SilentLogger,
+} from "./logger";
+export type { Logger, LogLevel, LoggerOptions } from "./logger";
