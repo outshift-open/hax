9f90e20c8fde62468b70bb4fa9b17bba2715a0ef
Chizuzu <izuzuchidumga@gmail.com>
2025-10-02 16:32:44 -0700

    add license header to all files

--- hax/artifacts/code-editor/description.ts
+++ hax/artifacts/code-editor/description.ts
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2025 Cisco Systems, Inc. and its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export const CODE_EDITOR_DESCRIPTION =
   `Use code editors for displaying, editing, and sharing code snippets with syntax highlighting and formatting. Best for documentation, tutorials, code reviews, examples, and interactive coding experiences. Supports multiple programming languages with appropriate syntax highlighting.
 
--- hax/artifacts/mindmap/description.ts
+++ hax/artifacts/mindmap/description.ts
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2025 Cisco Systems, Inc. and its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export const MINDMAP_DESCRIPTION =
   `Use mindmaps to visualize hierarchical relationships and branching concepts. Best for showing how ideas connect and flow from central themes. Ideal for brainstorming sessions, project planning, or concept mapping. Root nodes automatically get special styling (larger, blue border) to indicate hierarchy.
 
